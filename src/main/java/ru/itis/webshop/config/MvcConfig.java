ec0ee72fea464d2120de6548995dc260ac3e432b
Aina <ainaard@mail.ru>
2020-10-25 15:48:46 +0300

    cleaned config

--- src/main/java/ru/itis/webshop/config/MvcConfig.java
+++ src/main/java/ru/itis/webshop/config/MvcConfig.java
@@ -12,11 +12,9 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**", "/css/**", "/js/**", "/fonts/**")
+        registry.addResourceHandler("/images/**", "/css/**")
                 .addResourceLocations(
                         "classpath:/static/css/",
-                        "classpath:/static/js/",
-                        "classpath:/static/fonts/",
                         "classpath:/static/images/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("/webjars/")
